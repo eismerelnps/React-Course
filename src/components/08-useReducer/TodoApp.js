d3237f124c5e8f2373704e86e211ab6ac8420c88
Eismer Lobaina <eismerlobaina@gmail.com>
2023-05-05 14:53:03 -0400

    Delete button now is funtional, todos now are saved in local storage

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,19 +1,70 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 import { todoReducer } from "./todoReducer";
+import { useForm } from "../../hooks/useForm";
+
 import "./todoAppStyle.css";
 
-const initialState = [
-  {
-    id: new Date().getTime(),
-    desc: "Learn React",
-    done: false,
-  },
-];
+//to stablis the initial state of the reducer
+const init = () => {
+   return JSON.parse(localStorage.getItem('todos')) || [];
+};
 
 export const TodoApp = () => {
-  const [todos] = useReducer(todoReducer, initialState);
-  console.log(todos);
+  const [todos, dispatch] = useReducer(todoReducer, [], init);
 
+  //description is the formValues object desestructured
+  const [{ description }, handleInputChange, reset] = useForm({
+    description: "",
+  });
+
+
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //validating
+    if (description.trim().length <= 3) {
+      return;
+    }
+
+    //creating a new object with the datas of the new ToDo
+    const newTodo = {
+      id: new Date().getTime(),
+      desc: description,
+      done: false,
+    };
+    //creating a action for this ToDO
+    const action = {
+      type: "add",
+      payload: newTodo,
+    };
+    //Dispatch knows to who reducer send the information
+    dispatch(action);
+    reset();
+  };
+
+
+
+  //delete the todo by todo id
+  const handleDelete = (todoId) => {
+    //action of the call
+    const action = {
+      type: "remove",
+      payload: todoId
+    }
+    
+     // sends action to useReducer
+    dispatch( action );
+  };
+
+
+  //jsx code of the coponent
   return (
     <div>
       <h1>TodoApp ({todos.length})</h1>
@@ -27,7 +78,12 @@ export const TodoApp = () => {
                 <p className="text-center">
                   {i + 1}. {todo.desc}
                 </p>
-                <button className="btn btn-danger">Delete</button>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => handleDelete(todo.id)}
+                >
+                  Delete
+                </button>
               </li>
             ))}
           </ul>
@@ -36,18 +92,21 @@ export const TodoApp = () => {
         <div className="col-5">
           <h4>Add toDo</h4>
           <hr />
-          <form>
+          <form onSubmit={handleSubmit}>
             <input
               type="text"
               name="description"
               className="form-control"
               placeholder="Learn ...."
               autoComplete="off"
+              value={description}
+              onChange={handleInputChange}
             />
             <button
-            className="btn btn-outline-primary mt-1 btn-block"
+              type="submit"
+              className="btn btn-outline-primary mt-1 btn-block"
             >
-                ADD
+              ADD
             </button>
           </form>
         </div>
--- src/components/08-useReducer/todoReducer.js
+++ src/components/08-useReducer/todoReducer.js
@@ -1,3 +1,11 @@
-const todoReducer = ( state = [], action) => {
+export const todoReducer = (state = [], action) => {
+  switch (action.type) {
+    case "add":
+      return [...state, action.payload];
 
-}
+    case "remove":
+        return state.filter( todo => todo.id !== action.payload )
+    default:
+      return state;
+  }
+};
